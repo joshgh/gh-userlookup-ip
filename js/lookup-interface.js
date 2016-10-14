5644db791a0444bcaa600451dae5861e56ded176
Joshua Huffman <j.m.huffman@gmail.com>
2016-10-14 10:13:39 -0700

    retrieve and display users real name from github username

--- js/lookup-interface.js
+++ js/lookup-interface.js
@@ -1,11 +1,15 @@
-var Lookup = require('./../js/lookup.js').lookupModule;
-var apiKey = require('./../.env').apiKey;
+var lookup = require('./../js/lookup.js').lookupModule;
+
+function displayResponse(response){
+  $('#output').append("<p>" + response.name + "</p>");
+}
 
 $(document).ready(function(){
   $('#username-form').submit(function(event){
     event.preventDefault();
     var username = $('#username').val();
     console.log(username);
+    lookup(username, displayResponse);
 
   });
 
--- js/lookup.js
+++ js/lookup.js
@@ -1,7 +1,13 @@
-function Lookup (){
+var apiKey = require('./../.env').apiKey;
 
-};
+function lookup (username, cb){
+  $.get('https://api.github.com/users/' + username + '?access_token=' + apiKey).then(function(response){
+    cb(response);
+  }).fail(function(error){
+    console.log(error.responseJSON.message);
+  });
+}
 
-Lookup.prototype.
 
-exports.lookupModule = Lookup;
+
+exports.lookupModule = lookup;
